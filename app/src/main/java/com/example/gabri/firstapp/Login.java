d81846cca15fd3f6a394f0e9c3b8ce03eb97877b
Gabbo16 <gabri.bressan@gmail.com>
2017-11-07 19:33:46 +0100

    Aggiunto bottone Login e registrazione. Aggiunta inoltre Activity per Registration Form.

--- app/src/main/java/com/example/gabri/firstapp/Login.java
+++ app/src/main/java/com/example/gabri/firstapp/Login.java
@@ -1,7 +1,9 @@
 package com.example.gabri.firstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -9,12 +11,11 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+    }
 
-
-
-
-
-
+    public void registrationForm(View view){
+        Intent registrationActivity=new Intent(this, RegistrationForm.class);
+        startActivity(registrationActivity);
 
     }
 }
---
+++ app/src/main/java/com/example/gabri/firstapp/RegistrationForm.java
@@ -0,0 +1,13 @@
+package com.example.gabri.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistrationForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_form);
+    }
+}
