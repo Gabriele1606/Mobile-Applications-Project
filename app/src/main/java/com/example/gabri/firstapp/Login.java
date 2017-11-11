84cb58b045340a0c70ffeb2bbfde728dd54d503f
Simone <simone.desantis@mail.polimi.it>
2017-11-11 19:55:00 +0100

    Trasparency

--- app/src/main/java/com/example/gabri/firstapp/Login.java
+++ app/src/main/java/com/example/gabri/firstapp/Login.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 public class Login extends AppCompatActivity {
 
@@ -11,6 +12,16 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+            Window w= getWindow();
+            // in Activity's onCreate() for instance
+            w.getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
     }
 
     public void registrationForm(View view){
