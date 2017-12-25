c1cdcea309e2933a9f71b167fc862750aa432bac
Simone <simone.desantis@mail.polimi.it>
2017-12-25 12:37:38 +0100

    Revert "Timer reload"
    This reverts commit af2be6555c979dbc3383f83633623a26ded64caf.

--- app/src/main/java/com/example/gabri/firstapp/Controller/Login.java
+++ app/src/main/java/com/example/gabri/firstapp/Controller/Login.java
@@ -7,37 +7,15 @@ import android.view.View;
 import android.view.Window;
 
 import com.example.gabri.firstapp.R;
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
-import com.firebase.ui.auth.ResultCodes;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class Login extends AppCompatActivity {
-    private static final int RC_SIGN_IN = 123;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
             Window w= getWindow();
-        FirebaseApp.initializeApp(this);
-
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
-                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build());
-
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(providers)
-                        .build(),
-                RC_SIGN_IN);
-
-        // in Activity's onCreate() for instance
+            // in Activity's onCreate() for instance
             w.getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -52,25 +30,6 @@ public class Login extends AppCompatActivity {
         Intent registrationActivity=new Intent(this, RegistrationForm.class);
         startActivity(registrationActivity);
 
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
-
-            if (resultCode == ResultCodes.OK) {
-                // Successfully signed in
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                // ...
-            } else {
-                // Sign in failed, check response for error code
-                // ...
-            }
-        }
     }
 
     public void login(View view){
